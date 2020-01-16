58dbce2413f97f62c9abaf54de882450f8bc97f4
xiezhaofei <xiezhaofei@meituan.com>
2020-01-16 21:07:58 +0800

    代码优化

--- dist/js/weicat.js
+++ dist/js/weicat.js
@@ -12,14 +12,14 @@ var fatherID = null;
 var accessToken = null;
 
 if (code != null && code != undefined) {//网页授权获取code
-    if (sessionStorage['headimgurl']) {
-        headimgurl = sessionStorage['headimgurl'];
-        openid = sessionStorage['openid'];
-        nickname = sessionStorage['nickname'];
-        alert('headimgurl:${headimgurl};openid:${openid};nickname:${nickname}');
+    if (localStorage['headimgurl']) {
+        headimgurl = localStorage['headimgurl'];
+        openid = localStorage['openid'];
+        nickname = localStorage['nickname'];
+        // alert('headimgurl:' + headimgurl);
     } else {
-        //axiosRequest(code);
-        ajaxRequest(code);
+        axiosRequest(code);
+        //ajaxRequest(code);
     }
 } else {
     window.location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxc2912f4e5c9ea73b&redirect_uri=http%3a%2f%2fuss.sonicers.com%2fmouse%2findex.html&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect ';
@@ -39,15 +39,19 @@ function GetQueryString(name) {
 function axiosRequest(c) {
     axios.get("https://api.sonicers.com/release/get_user_info", {
         params: {
-            code: c,
-            reqUrl: window.location.href
+            code: c
         }
-    }).then(d => {
-        alert('+++++++++++++++++');
     }).then((res) => {
-        alert('===============');
+        let msg = res.data
+        headimgurl = msg.headimgurl;
+        openid = msg.openid;
+        nickname = msg.nickname;
+        localStorage['headimgurl'] = headimgurl;
+        localStorage['openid'] = openid;
+        localStorage['nickname'] = nickname;
+        //alert('headimgurl:' + headimgurl + ' ; nickname:' + nickname);
     }).catch((error) => {
-        alert('-----===========---');
+        alert('授权出错');
     })
 }
 
@@ -59,12 +63,12 @@ function ajaxRequest(code) {
         dataType: "json",
         success: function (msg) {
             //alert('success:openid=' + msg.openid + " ; headimgurl=" + msg.headimgurl);
-            headimgurl = msg.headimgurl.substr(0, msg.headimgurl.length - 1) + '132';
+            headimgurl = msg.headimgurl;
             openid = msg.openid;
             nickname = msg.nickname;
-            sessionStorage['headimgurl'] = headimgurl;
-            sessionStorage['openid'] = openid;
-            sessionStorage['nickname'] = nickname;
+            localStorage['headimgurl'] = headimgurl;
+            localStorage['openid'] = openid;
+            localStorage['nickname'] = nickname;
         },
         error: function (msg) {
             alert('授权出错');
