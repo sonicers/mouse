330083c7481d3e560c9d6dd37cee823e24e620e1
yourongcan <iponkan@gmail.com>
2020-01-23 11:41:06 +0800

    bugfix

--- dist/js/weicat.js
+++ dist/js/weicat.js
@@ -22,7 +22,7 @@ if (code != null && code != undefined) {//网页授权获取code
     }
 } else {
     //window.location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxc2912f4e5c9ea73b&redirect_uri=https%3a%2f%2fuss.sonicers.com%2fmouse%2findex.html&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect ';
-    window.location.replace('https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxc2912f4e5c9ea73b&redirect_uri=https%3a%2f%2fuss.sonicers.com%2fmouse%2findex.html&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect ');
+    window.location.replace ='https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxc2912f4e5c9ea73b&redirect_uri=https%3a%2f%2fuss.sonicers.com%2fmouse%2findex.html&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect ';
     // alert('headimgurl=====');
 }
 
